e65979d4375bdce6f6ea6ce74eb7e325c1688330
Alisson Rego <alissondroid07@gmail.com>
2024-03-26 12:38:30 -0300

    Add custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        "cyan": "#2acfcf",
+        "dark-violet": "#3b3054",
+
+        // Secondary
+        "red-10": "#f46262",
+
+        //Neutral
+        "gray-10": "#bfbfbf",
+        "grayish-violet": "#9e9aa7",
+        "very-dark-blue": "#35323e",
+        "very-dark-violet": "#232127"
+      }
+    },
   },
   plugins: [],
 }
