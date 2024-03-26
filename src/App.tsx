5c2dda692d484d1c351e18af629ab32f37711091
Alisson Rego <alissondroid07@gmail.com>
2024-03-26 12:30:51 -0300

    Add Home and Navigation components / font change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Home from "./modules/Home"
+
 function App() {
 
   return (
     <>
+      <Home />
     </>
   )
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <div className="bg-white">
+        <Navigation />
+    </div>
+  )
+}
+
+export default Navigation
