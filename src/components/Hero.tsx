62304fec895cc045be87bbb393fb45a07a20b10a
Alisson Rego <alissondroid07@gmail.com>
2024-03-28 13:58:45 -0300

    Changes in the Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,10 @@ import IllustrationWorking from "../assets/IllustrationWorking"
 
 const Hero = () => {
   return (
-    <div className="w-full mt-20 flex flex-col items-center">
-        <div className="w-full overflow-hidden">
-          <IllustrationWorking className="scale-75 -translate-x-12 -translate-y-10" />
-        </div>
+    <div className="w-full mt-20 flex flex-col items-center overflow-hidden">
+        <IllustrationWorking className="scale-75 translate-x-20" />
 
-        <div className="flex flex-col items-center gap-8 mx-4">
+        <div className="flex flex-col items-center gap-6 mx-6">
           <p className="text-4xl text-center text-very-dark-blue font-bold">More than just shorter links</p>
 
           <p className="text-center text-gray-10">Build your brand's recognition and get detailed insights on how your links are performing.</p>
